8ecf0c89688f7299b536ff0b4e9ca5ab0b7ffb0f
Benjamin Elland Soderberg <beelso@proton.me>
2025-12-07 13:04:08 +0100

    Day four Completed

--- AdventOfCode2025.Tests/DayFour/SolverTest.cs
+++ AdventOfCode2025.Tests/DayFour/SolverTest.cs
@@ -1,10 +1,130 @@
+using System.Collections.Immutable;
+using Shouldly;
+
 namespace AdventOfCode2025.DayFour;
 
 public class SolverTest
 {
    [Fact]
-   public void GivenInput_WhenLoading_ThenBoolArray()
+   public void GivenInput_WhenLoading_ThenArrayOfBoolArray()
    {
+      var input = ImmutableArray.Create<string>(
+         "..@@",
+         "@@@.",
+         "@@@@");
+
+      var res = Solver.TooPaperRollMatrix(input);
+
+      bool[,] expected = { { false, false, true, true }, { true, true, true, false }, { true, true, true, true } };
+      res.ShouldBe(expected);
+   }
+
+   [Fact]
+   public void GivenCoordinateWithFewerThanFourAdjacentTrueValues_WhenValidatingForkliftAccess_ThenTrue()
+   {
+      var sut = new Solver();
+      bool[,] matrix = { { true, true, true }, { false, true, true }, { false, false, false } };
+
+      sut.ValidateForkliftAccess(new(0, 0), matrix).ShouldBeTrue();
+   }
+
+   [Fact]
+   public void GivenCoordinate_WhenGettingAdjacentCoordinates_ThenAdjacentCoordinates()
+   {
+      var coordinate = new Coordinate(1, 1);
       
+      var res = Solver.CreateAdjenctCoordinates(coordinate);
+      
+      res.ShouldBe([
+         new(0, 0), new(1, 0), new(2, 0), 
+         new(0, 1), new(2, 1),
+         new(0, 2), new(1, 2), new(2, 2)]);
+   }
+
+   [Fact]
+   public void GivenCoordinateWithFourAdjacentTrueValues_WhenValidatingForkliftAccess_ThenFalse()
+   {
+      var sut = new Solver();
+      bool[,] matrix= { { true, true, true }, { false, true, true }, { false, false, false } };
+
+      sut.ValidateForkliftAccess(new(1, 1), matrix) .ShouldBeFalse();
+   }
+
+   [Fact]
+   public void GivenPaperRollMatrix_WhenCountingRollsAccessibleForForklift_ThenAccessible()
+   {
+      var sut = new Solver();
+      var input = ImmutableArray.Create<string>(
+         "..@@.@@@@.",
+         "@@@.@.@.@@",
+         "@@@@@.@.@@",
+         "@.@@@@..@.",
+         "@@.@@@@.@@",
+         ".@@@@@@@.@",
+         ".@.@.@.@@@",
+         "@.@@@.@@@@",
+         ".@@@@@@@@.",
+         "@.@.@@@.@.");
+      var matrix = Solver.TooPaperRollMatrix(input);
+      
+      var res = sut.RemoveAccessibleRolls(matrix);
+      
+      res.Removed.ShouldBe(13);
+   }
+
+   [Fact]
+   public void GivenPartOne_WhenSolving_ThenSolve()
+   {
+      var sut = new Solver();
+
+      var res = sut.PartOne();
+      
+      res.ShouldBe("1419");
+   }
+   
+   
+
+   [Fact]
+   public void GivenPaperRollMatrix_WhenRemovingPaperRolls_ThenMatrixWithoutRemovedRolls()
+   {
+      var sut = new Solver();
+      var input = ImmutableArray.Create<string>(
+         "..@@.@@@@.",
+         "@@@.@.@.@@",
+         "@@@@@.@.@@",
+         "@.@@@@..@.",
+         "@@.@@@@.@@",
+         ".@@@@@@@.@",
+         ".@.@.@.@@@",
+         "@.@@@.@@@@",
+         ".@@@@@@@@.",
+         "@.@.@@@.@.");
+      var matrix = Solver.TooPaperRollMatrix(input);
+      
+      var res = sut.RemoveAccessibleRolls(matrix);
+
+      var expected = Solver.TooPaperRollMatrix([
+         ".......@..",
+         ".@@.@.@.@@",
+         "@@@@@...@@",
+         "@.@@@@..@.",
+         ".@.@@@@.@.",
+         ".@@@@@@@.@",
+         ".@.@.@.@@@",
+         "..@@@.@@@@",
+         ".@@@@@@@@.",
+         "....@@@..."
+      ]);
+      res.ShouldBeEquivalentTo(new RollState(expected, 13));
+   }
+
+   [Fact]
+   public void GivenPart2_WhenSolving_ThenSolved()
+   {
+      var sut = new Solver();
+
+      var res = sut.PartTwo();
+      
+      res.ShouldBe("8739");
    }
 }
--- AdventOfCode2025/DayFour/Solver.cs
+++ AdventOfCode2025/DayFour/Solver.cs
@@ -1,14 +1,106 @@
+using System.Collections.Immutable;
+
 namespace AdventOfCode2025.DayFour;
 
 public class Solver : ISolver
 {
     public string PartOne()
     {
-        throw new NotImplementedException();
+        var input = Input.Load("DayFour");
+        
+        var paperRollMatrix = TooPaperRollMatrix(input);
+        
+        return RemoveAccessibleRolls(paperRollMatrix).Removed.ToString();
     }
 
     public string PartTwo()
     {
-        throw new NotImplementedException();
+        var input = Input.Load("DayFour");
+
+        var paperRollMatrix = TooPaperRollMatrix(input);
+
+        var totalRemoved = 0;
+        var currentState = new RollState(paperRollMatrix, 0);
+        do
+        {
+            var newState = RemoveAccessibleRolls(currentState.State);
+            currentState = newState;
+            totalRemoved += newState.Removed;
+        } while (currentState.Removed != 0);
+
+        return totalRemoved.ToString();
+    }
+
+    public static bool[,] TooPaperRollMatrix(ImmutableArray<string> input)
+    {
+        var rows = input.Length;
+        var cols = input[0].Length;
+
+        var matrix = new bool[rows, cols];
+        for (var r = 0; r < rows; r++)
+        {
+            for (var c = 0; c < cols; c++)
+            {
+                matrix[r, c] = input[r][c] == '@';
+            }
+        }
+        return matrix;
+    }
+
+    public bool ValidateForkliftAccess(Coordinate coordinate, bool[,] input)
+    {
+        var width = input.GetLength(0);
+        var height = input.GetLength(1);
+
+        var validAdjacetCoordinates = CreateAdjenctCoordinates(coordinate)
+            .Where(x => x is { X: >= 0, Y: >= 0, } && x.X < width && x.Y < height);
+        var res = validAdjacetCoordinates.Select(x => input[x.X, x.Y]);
+        return res.Count(x => x) < 4;
+    }
+
+    public static ImmutableArray<Coordinate> CreateAdjenctCoordinates(Coordinate c)
+    {
+        var builder = ImmutableArray.CreateBuilder<Coordinate>();
+        foreach (var offset in Offsets)
+        {
+            builder.Add(new(c.X + offset.X, c.Y + offset.Y));
+        }
+        return builder.ToImmutable();
+    }
+
+    static readonly Coordinate[] Offsets =
+    {
+        new(-1, -1), new(0, -1), new(1, -1),
+        new(-1, 0), new(1, 0),
+        new(-1, 1), new(0, 1), new(1, 1),
+    };
+
+    public RollState RemoveAccessibleRolls(bool[,] matrix)
+    {
+        var updatedRolls = (bool [,])matrix.Clone();
+        var count = 0;
+        for (var x = 0; x < matrix.GetLength(0); x++)
+        {
+            for (var y = 0; y < matrix.GetLength(1); y++)
+            {
+                if (matrix[x, y])
+                {
+                    if (ValidateForkliftAccess(new(x, y), matrix))
+                    {
+                        count++;
+                        updatedRolls[x, y] = false;
+                    }      
+                }
+               
+            }
+        }
+        return new(updatedRolls, count);
     }
 }
+public record Coordinate(
+    int  X, 
+    int Y);
+    
+public record RollState(
+        bool [,] State, 
+        int Removed);
